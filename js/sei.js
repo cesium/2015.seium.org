3a98f8f30d123fca88f28fd3d1b9a1495a42e761
Fernando Mendes <devfrmendes@gmail.com>
2015-01-30 23:40:40 +0000

    Did stuff... I guess?

--- js/sei.js
+++ js/sei.js
@@ -1,24 +1,24 @@
 angular.module('sei-app',['angular-carousel'])
 .controller('eventCtrl',['$scope','$rootScope',function($scope,$rootScope){
     /*$scope.eventSlides = [
-        {file:'event/14-fev.html', day:'14 Fev', weekday:'Sábado'},
-        {file:'event/15-fev.html', day:'15 Fev', weekday:'Domingo'},
-        {file:'event/16-fev.html', day:'16 Fev', weekday:'Segunda'},
-        {file:'event/17-fev.html', day:'17 Fev', weekday:'Terça'},
-        {file:'event/18-fev.html', day:'18 Fev', weekday:'Quarta'},
-        {file:'event/19-fev.html', day:'19 Fev', weekday:'Quinta'},
-        {file:'event/20-fev.html', day:'20 Fev', weekday:'Sexta'},
-        {file:'event/21-fev.html', day:'21 Fev', weekday:'Sabado'}
+        {file:'events/14-fev.html', day:'14 Fev', weekday:'Sábado'},
+        {file:'events/15-fev.html', day:'15 Fev', weekday:'Domingo'},
+        {file:'events/16-fev.html', day:'16 Fev', weekday:'Segunda'},
+        {file:'events/17-fev.html', day:'17 Fev', weekday:'Terça'},
+        {file:'events/18-fev.html', day:'18 Fev', weekday:'Quarta'},
+        {file:'events/19-fev.html', day:'19 Fev', weekday:'Quinta'},
+        {file:'events/20-fev.html', day:'20 Fev', weekday:'Sexta'},
+        {file:'events/21-fev.html', day:'21 Fev', weekday:'Sabado'}
     ];*/
     $scope.eventSlides = [
-        {file:'event/17-fev.html', day:'14 Fev', weekday:'Sábado'},
-        {file:'event/17-fev.html', day:'15 Fev', weekday:'Domingo'},
-        {file:'event/17-fev.html', day:'16 Fev', weekday:'Segunda'},
-        {file:'event/17-fev.html', day:'17 Fev', weekday:'Terça'},
-        {file:'event/17-fev.html', day:'18 Fev', weekday:'Quarta'},
-        {file:'event/17-fev.html', day:'19 Fev', weekday:'Quinta'},
-        {file:'event/17-fev.html', day:'20 Fev', weekday:'Sexta'},
-        {file:'event/17-fev.html', day:'21 Fev', weekday:'Sabado'}
+        {file:'events/17-fev.html', day:'14 Fev', weekday:'Sábado'},
+        {file:'events/17-fev.html', day:'15 Fev', weekday:'Domingo'},
+        {file:'events/17-fev.html', day:'16 Fev', weekday:'Segunda'},
+        {file:'events/17-fev.html', day:'17 Fev', weekday:'Terça'},
+        {file:'events/17-fev.html', day:'18 Fev', weekday:'Quarta'},
+        {file:'events/17-fev.html', day:'19 Fev', weekday:'Quinta'},
+        {file:'events/17-fev.html', day:'20 Fev', weekday:'Sexta'},
+        {file:'events/17-fev.html', day:'21 Fev', weekday:'Sabado'}
     ];
     $scope.carouselEvents = 0;
 
